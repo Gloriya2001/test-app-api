ac6461c1cb51c0a1e39202a82a2c6741bb8a3af5
Gloriya2001 <glooriaa2001@gmail.com>
2024-08-21 20:17:06 +0530

    User Sign in complted

--- app.js
+++ app.js
@@ -12,6 +12,37 @@ app.use(cors())//security purpose
 
 mongoose.connect("mongodb+srv://gloria2001:gloria2001@cluster0.ipg35w1.mongodb.net/testappdb?retryWrites=true&w=majority&appName=Cluster0")
 //connnects our system and mongo cloud
+
+//User Sign In 
+app.post("/signin",async(req,res)=>{
+    let input = req.body
+    let result = userModel.find({email:req.body.email}).then(
+        (items)=>{
+            if (items.length>0) {
+                let passwordValidator = bcrypt.compareSync(req.body.password,items[0].password)
+                if (passwordValidator) {
+                    jwt.sign({email:req.body.email},
+                            "testapp",
+                            {expiresIn:"1d"},
+                            (error,token)=>{
+                                if (error) {
+                                    res.json({"status":"error","errorMessage":error})
+                                } else {
+                                    res.json({"status":"success","token":token,"userId":items[0]._id})
+                                }
+                            }
+                        )
+                } else {
+                    res.json({"status":"Incorrect Passoword"})
+                }
+            } else {
+                res.json({"status":"Invalid email"})
+            }
+        }
+    )
+})
+
+//User Sign Up
 app.post("/signup", async(req,res)=>{
 
     let input = req.body //collecting input
